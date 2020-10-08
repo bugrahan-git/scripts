4469f488d9d17fe5a3e2bbdfad68e6700a8c0990
bugrahan <umutbugrahanozkan@gmail.com>
2020-10-08 21:39:13 +0300

    initial commit

---
+++ gitlab-create-repo.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+mkdir $1 && cd $1 1>$2 2>/dev/null
+
+if [[ $? != 0 ]]; then
+	exit 1
+fi
+
+
+echo "# $1" > README.md
+
+git init
+git add .
+git remote add origin git@gitlab.com:bugrahan/$1.git
+git commit -m "add file README.md"
+git push origin master
